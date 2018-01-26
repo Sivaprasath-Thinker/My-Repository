d02b5b10b9d01e628c10da0952666c544bb424dc
Sivaprasath-Thinker <35728568+Sivaprasath-Thinker@users.noreply.github.com>
2018-01-26 13:30:31 +0530

    Create oddeven.c

---
+++ oddeven.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter the value");
+scanf("%d",&a);
+if(a>=30)
+{
+printf("value is even");
+}
+else if(a==0)
+{
+printf("value is zero");
+}
+else
+{
+    printf("value is odd");
+}
+}
