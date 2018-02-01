11db7ca651abfc7a561dc2d822271804b0c26321
Sivaprasath-Thinker <35728568+Sivaprasath-Thinker@users.noreply.github.com>
2018-02-01 16:31:38 +0530

    Create leapyearornot.c

---
+++ leapyearornot.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a;
+    printf("enter the value=");
+    scanf("%d",&a);
+    if(a%4==0)
+    {
+        printf("it is leap year");
+    }
+    else 
+    {
+        printf("it is not leap year");
+    }
+}
