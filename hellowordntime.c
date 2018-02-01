759634137358d20071dd931d4e639c1b8ae4305e
Sivaprasath-Thinker <35728568+Sivaprasath-Thinker@users.noreply.github.com>
2018-02-01 16:45:51 +0530

    Create hellowordntime.c

---
+++ hellowordntime.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,i;
+    printf("enter the value=");
+    scanf("%d",&a);
+    for(i=0;i<a;i++)
+    {
+        printf("hello");
+    }
+}
