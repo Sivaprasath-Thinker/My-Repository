95e69ffb13f7a05ddc84f3255870cdae6d9cfea5
Sivaprasath-Thinker <35728568+Sivaprasath-Thinker@users.noreply.github.com>
2018-03-01 23:59:56 +0530

    Create largenumfromarray.c

---
+++ largenumfromarray.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int a,i,max;
+    int array[a];
+    printf("enter the number of array:");
+    scanf("%d",&a);
+    printf("enter the array:");
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&array[i]);
+        max=array[0];
+    }
+    for(i=0;i<a;i++)
+    {
+        if(array[i]>max)
+        {
+            max=array[i];
+        }
+    }
+    printf("%d",max);
+}
