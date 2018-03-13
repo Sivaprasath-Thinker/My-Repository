1b5e38674ba44564f618efbe4371b8f14d214147
Sivaprasath-Thinker <35728568+Sivaprasath-Thinker@users.noreply.github.com>
2018-03-13 03:50:54 -0700

    Create sortedarray.c

---
+++ sortedarray.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+    int n,a[10],i,j,temp;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=1;i<=n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    for(i=1;i<=n;i++)
+        printf("%d\t",a[i]);
+    
+}
